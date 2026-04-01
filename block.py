536fb4460bc569f66c52c13d1ea4d7758874b337
liu-kenny <liu.kenny@northeastern.edu>
2026-04-01 19:12:17 -0400

    implemented get_tiles()

--- block.py
+++ block.py
@@ -25,8 +25,53 @@ class Block:
         self.y = y
         self.orientation = orientation
 
-    def move(action):
-        
+    # updates block's anchor position (top-left most tile) based on action taken 
+    # and current orientation
+    def move(self, action):
+        x, y, o = self.x, self.y, self.orientation
+
+        if o == UPRIGHT:
+            if action == UP:
+                return Block(x - 2, y, VERTICAL)
+            elif action == DOWN:
+                return Block(x + 1, y, VERTICAL)
+            elif action == LEFT:
+                return Block(x, y - 2, HORIZONTAL)
+            elif action == RIGHT:
+                return Block(x, y + 1, HORIZONTAL)
+        elif o == VERTICAL:
+            if action == UP:
+                return Block(x - 1, y, UPRIGHT)
+            elif action == DOWN:
+                return Block(x + 2, y, UPRIGHT)
+            elif action == LEFT:
+                return Block(x, y - 1, VERTICAL)
+            elif action == RIGHT:
+                return Block(x, y + 1, VERTICAL)
+        elif o == HORIZONTAL:
+            if action == UP:
+                return Block(x - 1, y, HORIZONTAL)
+            elif action == DOWN:
+                return Block(x + 1, y, HORIZONTAL)
+            elif action == LEFT:
+                return Block(x, y - 1, UPRIGHT)
+            elif action == RIGHT:
+                return Block(x, y + 2, UPRIGHT)
+    
+    # returns a list of all tiles covered by a block based on its
+    # anchor position (top-left most tile) and orientation
+    def get_tiles(self):
+        x, y, o = self.x, self.y, self.orientation
+        tiles_covered = []
+        tiles_covered.append((x, y))
+        if o == UPRIGHT: 
+            return tiles_covered
+        elif o == VERTICAL:
+            tiles_covered.append((x + 1, y))
+        elif o == HORIZONTAL:
+            tiles_covered.append((x, y + 1))
+        return tiles_covered
+
 
         
 
